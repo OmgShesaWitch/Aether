b002b6a86ce9c8a6977882a6a10ace51cddc37aa
mavi <mavisberrye@gmail.com>
2026-05-17 19:31:35 -0300

    feat: implement dynamic city climate search.

--- main.py
+++ main.py
@@ -2,14 +2,34 @@ import requests
 import pandas as pd
 import json
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=-22.9068&longitude=-43.1729&current=temperature_2m,relative_humidity_2m,precipitation,cloud_cover,wind_speed_10m,wind_direction_10m&timezone=America%2FSao_Paulo"
+cidade = input("Digite o nome da cidade: ").capitalize()
+
+geocoding = f"https://geocoding-api.open-meteo.com/v1/search?name={cidade}&count=10&language=en&format=json"
+
+
+local = requests.get(geocoding)
+
+dados = local.json()
+
+#-------------Cria o arquivo Json para leitura mais fácil---------------
+
+with open("Coordenadas.json", "w", encoding="utf-8") as arquivo:
+    json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+
+#------------------------------------------------------------------------
+
+latitude = dados["results"][0]["latitude"]
+longitude = dados["results"][0]["longitude"]
+
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,relative_humidity_2m,precipitation,cloud_cover,wind_speed_10m,wind_direction_10m&timezone=America%2FSao_Paulo"
 resposta = requests.get(url)
 
 data = resposta.json()
 
-#----Cria o arquivo Json para leitura mais fácil-----
+#-------------Cria o arquivo Json para leitura mais fácil---------------
+
 with open("dados_climaticos.json", "w", encoding="utf-8") as arquivo:
-    json.dump(data, arquivo, indent=4, ensure_ascii=False) 
+    json.dump(data, arquivo, indent=4, ensure_ascii=False)
 
 #------------------------------------------------------------------------
 
