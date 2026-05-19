d3557da5c91e3f75c511cf553c60206722d3e425
mavi <mavisberrye@gmail.com>
2026-05-19 19:44:00 -0300

    refactor: reorganize main workflow

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
-import requests
-import pandas as pd
-import json
 from api import buscar_coordenadas, buscar_dados
 from analysis import chuva
+from ErrorTreatment import inputinvalido
 
-cidade = input("Digite o nome da cidade: ").capitalize()
 
-latitude, longitude, dados = buscar_coordenadas(cidade)
 
-tabela, clima, data = buscar_dados(latitude, longitude)
+while True:
 
-previsao = chuva(clima["relative_humidity_2m"], clima["cloud_cover"], clima["precipitation"])
+    cidade = input("Digite o nome da cidade: ").capitalize()
 
-print("--------------Previsão-------------- \n     ", previsao)
+    if cidade == "0":
+        break
 
-horario = clima["time"]
-temperatura = clima["temperature_2m"]
-windspeed = clima["wind_speed_10m"]
-dirvento = clima["wind_direction_10m"]
-umidade = clima["relative_humidity_2m"]
-probchuva = clima["precipitation"]
-cloudcover = clima["cloud_cover"]
+    erro = inputinvalido(cidade)
+    
+    if erro:
+        print(erro)
+        cidade = input("Digite o nome da cidade: ").capitalize()
 
-print(("Horário: "), horario.replace("T", " "))
-print(("Temperatura: "),temperatura, "°C")
-print(("Velocidade do vento: "),windspeed, "km/h")
-print(("Direção do vento: "),dirvento, "°")
-print(("Umidade: "),umidade, "%")
-print(("Chuva: "),probchuva, "%")
-print(("Cobertura de nuvens: "),cloudcover, "%")
+    resultado = buscar_coordenadas(cidade)
+
+    if resultado is None:
+        exit()
+
+    latitude, longitude, dados = resultado
+
+    tabela, clima, data = buscar_dados(latitude, longitude)
+
+    previsao = chuva(clima["relative_humidity_2m"], clima["cloud_cover"], clima["precipitation"])
+
+    print("--------------Previsão-------------- \n     ", previsao)
+
+    horario = clima["time"]
+    temperatura = clima["temperature_2m"]
+    windspeed = clima["wind_speed_10m"]
+    dirvento = clima["wind_direction_10m"]
+    umidade = clima["relative_humidity_2m"]
+    probchuva = clima["precipitation"]
+    cloudcover = clima["cloud_cover"]
+
+    print(("Horário: "), horario.replace("T", " "))
+    print(("Temperatura: "),temperatura, "°C")
+    print(("Velocidade do vento: "),windspeed, "km/h")
+    print(("Direção do vento: "),dirvento, "°")
+    print(("Umidade: "),umidade, "%")
+    print(("Chuva: "),probchuva, "%")
+    print(("Cobertura de nuvens: "),cloudcover, "%")
