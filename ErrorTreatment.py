e2d6e53ba6b178b40b960ccc9c686ddbb6d16e98
mavi <mavisberrye@gmail.com>
2026-06-05 19:35:18 -0300

    chore: remove unnecessary file

--- ErrorTreatment.py
+++
@@ -1,8 +0,0 @@
-def cidadenaoencontrada():
-    return "Cidade não encontrada!"
-
-def inputinvalido(entrada):
-    if entrada.strip() == "":
-        return "Por favor, digite o nome de uma cidade."
-    if entrada.isdigit() == True:
-        return "Por favor, digite o nome da cidade."
