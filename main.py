58e02736d5fc12e41fbf34cca84a0a9b5ea4a654
mavi <mavisberrye@gmail.com>
2026-06-05 19:19:35 -0300

    feat: use weather checks in main flow

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
 from api import buscar_coordenadas, buscar_dados
-from analysis import chuva
-from ErrorTreatment import inputinvalido
+from analysis import analisar_chuva, avisos
+from validacoes import validar_cidade
 
 
 
 while True:
 
-    cidade = input("Digite o nome da cidade: ").capitalize()
+    cidade = input("Digite o nome da cidade ou 0 para encerrar o programa: ").capitalize()
 
     if cidade == "0":
+        print("Programa encerrado...")
         break
 
-    erro = inputinvalido(cidade)
+    erro = validar_cidade(cidade)
     
     if erro:
         print(erro)
-        cidade = input("Digite o nome da cidade: ").capitalize()
+        continue
 
     resultado = buscar_coordenadas(cidade)
 
     if resultado is None:
-        exit()
+        print("Cidade não encontrada ou erro na busca!")
+        continue
 
     latitude, longitude, dados = resultado
 
     tabela, clima, data = buscar_dados(latitude, longitude)
 
-    previsao = chuva(clima["relative_humidity_2m"], clima["cloud_cover"], clima["precipitation"])
+    previsao = analisar_chuva(clima["relative_humidity_2m"], clima["cloud_cover"], clima["precipitation"])
 
     print("--------------Previsão-------------- \n     ", previsao)
 
     horario = clima["time"]
     temperatura = clima["temperature_2m"]
-    windspeed = clima["wind_speed_10m"]
+    velocidadevento = clima["wind_speed_10m"]
     dirvento = clima["wind_direction_10m"]
     umidade = clima["relative_humidity_2m"]
-    probchuva = clima["precipitation"]
-    cloudcover = clima["cloud_cover"]
+    precipitacao = clima["precipitation"]
+    nuvens = clima["cloud_cover"]
+    rajadas = clima["wind_gusts_10m"]
 
     print(("Horário: "), horario.replace("T", " "))
     print(("Temperatura: "),temperatura, "°C")
-    print(("Velocidade do vento: "),windspeed, "km/h")
+    print(("Velocidade do vento: "),velocidadevento, "km/h")
     print(("Direção do vento: "),dirvento, "°")
     print(("Umidade: "),umidade, "%")
-    print(("Chuva: "),probchuva, "%")
-    print(("Cobertura de nuvens: "),cloudcover, "%")
+    print(("Chuva: "),precipitacao, "%")
+    print(("Cobertura de nuvens: "),nuvens, "%")
+    print("Rajadas de vento: ", rajadas, "km/h")
+
+    avisosextras = avisos(clima["wind_gusts_10m"], clima["relative_humidity_2m"])
+
+    if avisosextras != None:
+        print(avisosextras)
