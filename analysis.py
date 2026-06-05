2dbce9b8cc5d1768b9a40d4081042fea9089aba8
mavi <mavisberrye@gmail.com>
2026-06-05 19:15:17 -0300

    feat: add validation for strong wind and low humidity conditions

--- analysis.py
+++ analysis.py
@@ -1,4 +1,4 @@
-def chuva(umidade, nuvens, precipitacao):
+def analisar_chuva(umidade, nuvens, precipitacao):
     if precipitacao > 0:
        return "Já está chovendo!!"
     elif umidade > 80 and nuvens > 85:
@@ -10,3 +10,14 @@ def chuva(umidade, nuvens, precipitacao):
     else:
         return "Baixa chance de chuva"
     
+def avisos(velvento, umidade):
+    if velvento >= 30:
+        print("Parece que o vento está relativamente forte. Contraindicado sair de casa.")
+    elif velvento >= 50:
+        print("O vento está forte. Veículos altos, ciclistas e embarcações não devem sair.")
+    elif velvento > 70:
+        print("O vento está muito forte!!! Abrigue-se imediatamente.")
+    if umidade <= 30 and umidade > 13:
+        print("A umidade está baixa. Mantenha-se hidratado.")
+    elif umidade <= 13:
+        print("O clima está extremamente seco! Beba água e mantenha-se atento a sua saúde!!!") 
